94b091963801c4ac5c95e72a3ca64087c066e54b
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-06 21:56:41 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Lori Rothermel on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
